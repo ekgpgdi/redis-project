4e79c1fb953ed9c13ac58696c75bf4ae6f2eb2c7
ekgpgdi <dlekgp0423@naver.com>
2025-03-25 23:19:09 +0900

    feat : Pub/Sub을 이용한 Redis 채팅방 기능 구현

--- src/main/java/dahye/project/redis/RedisProjectApplication.java
+++ src/main/java/dahye/project/redis/RedisProjectApplication.java
@@ -1,13 +1,26 @@
 package dahye.project.redis;
 
+import dahye.project.redis.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class RedisProjectApplication {
+public class RedisProjectApplication implements CommandLineRunner {
+
+    private final ChatService chatService;
 
     public static void main(String[] args) {
         SpringApplication.run(RedisProjectApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("Application started.. ");
+
+        chatService.enterChatRoom("chat1");
+    }
 }
---
+++ src/main/java/dahye/project/redis/chat/config/ChatRedisConfig.java
@@ -0,0 +1,24 @@
+package dahye.project.redis.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+public class ChatRedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+
+        return container;
+    }
+}
