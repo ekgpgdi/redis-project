ae93cd92fe3a7e5de504ce47830ba546de93283b
ekgpgdi <dlekgp0423@naver.com>
2025-03-25 18:00:51 +0900

    feat: redis 를 활용한 분산환경에서의 세션 스토어

--- src/main/java/dahye/project/redis/SessionLoginController.java
+++ src/main/java/dahye/project/redis/SessionLoginController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SessionLoginController {
 
+    /*
+        java -Dserver.port=8081 -jar redis-project-0.0.1-SNAPSHOT.jar
+        java -Dserver.port=8080 -jar redis-project-0.0.1-SNAPSHOT.jar
+
+        서로 다른 포트로 어플리케이션을 실행하고 요청을 진행해도 같은 세션 ID 로 값을 받아올 수 있음
+     */
+
     @GetMapping("/login")
     public String login(HttpSession session,
                         @RequestParam String name) {
