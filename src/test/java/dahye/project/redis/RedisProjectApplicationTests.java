de809bb8bdfd8687a25218621706f9babb1aa563
ekgpgdi <dlekgp0423@naver.com>
2025-03-25 12:25:15 +0900

    init commit

---
+++ src/test/java/dahye/project/redis/RedisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dahye.project.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
